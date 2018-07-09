e396b0d1309ea8635c77d9006b3174644b3d1c26
Gaberu <gabrielrosadn@gmail.com>
2018-07-08 23:21:16 -0300

    develop Add initial structure of movies collection view and cell
    -
    
    Signed-off-by: Gaberu <gabrielrosadn@gmail.com>

---
+++ MovieInvolved/Application/Common/MoviesCollectionView/Cell/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  MovieInvolved
+//
+//  Created by Gabriel Rosa on 08/07/18.
+//  Copyright © 2018 Gabe. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    public static let NIB_NAME   = "MovieCell"
+    public static let IDENTIFIER = "movieCellIdentifier"
+    
+    func configureView(movie: MovieDto) {
+        
+    }
+    
+}
---
+++ MovieInvolved/Application/Common/MoviesCollectionView/MoviesCollectionView.swift
@@ -0,0 +1,55 @@
+//
+//  MoviesCollectionView.swift
+//  MovieInvolved
+//
+//  Created by Gabriel Rosa on 08/07/18.
+//  Copyright © 2018 Gabe. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCollectionView: UICollectionView, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    var movies = [MovieDto]()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let movieCell = UINib(nibName: MovieCell.NIB_NAME, bundle: nil)
+        self.register(movieCell, forCellWithReuseIdentifier: MovieCell.IDENTIFIER)
+        
+        self.delegate   = self
+        self.dataSource = self
+    }
+    
+    func set(movies: [MovieDto]) {
+        self.movies = movies
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = self.dequeueReusableCell(withReuseIdentifier: MovieCell.IDENTIFIER, for: indexPath as IndexPath) as! MovieCell
+        
+        let movieDto = movies[indexPath.row]
+        
+        cell.configureView(movie: movieDto)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200,
+                      height: 210)
+    }
+}
+
